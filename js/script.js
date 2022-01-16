926ee66c914a8823f6881991dceb9c3f3f3af5a6
Jesse Pantoja <pantojabruin@gmail.com>
2022-01-15 18:09:24 -0800

    Implement nonstyled features of UI based rps

--- js/script.js
+++ js/script.js
@@ -1,28 +1,33 @@
-let playerScore = 0
-let computerScore = 0
+let playerScore = 0;
+let computerScore = 0;
+let roundNumber = 1;
+
+let roundResultDiv = document.querySelector('.round-result-text-container')
+let playerScoreSpan = document.querySelector('.player-score')
+let computerScoreSpan = document.querySelector('.computer-score')
+let playerButtonsContainer = document.querySelector('.player-buttons-container')
+let resetGameButton = document.querySelector('.reset-game-btn')
+let roundNumberSpan = document.querySelector('.round-number')
 
 function computerPlay() {
-    const computerChoices = ['rock', 'paper', 'scissors']
-    return computerChoices[Math.floor(Math.random() * 3)]
-}
+    const computerChoices = ['rock', 'paper', 'scissors'];
+    return computerChoices[Math.floor(Math.random() * 3)];
+};
 
 function playRound(playerSelection, computerSelection){
 
     if(playerSelection === computerSelection){
-         console.log(`It's a tie this round! Both you and the computer chose ${playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1)}.`)
          return 'tie'
     }
-
     else if (didPlayerWin(playerSelection, computerSelection)){
-         console.log(`You win this round! ${playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1)} beats ${computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1)}.`)
          return 'player'
     } else {
-         console.log(`You lose this round. ${computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1)} beats ${playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1)}.`)
          return 'computer'
-    }
-}
+    };
+};
 
 function didPlayerWin(playerSelection, computerSelection){
+    // For each of the 3 possible player choices, there is only one counterpart computer chioce which results in a win for the player
     switch (playerSelection) {
         case 'rock':
             return computerSelection === 'scissors'
@@ -30,36 +35,49 @@ function didPlayerWin(playerSelection, computerSelection){
             return computerSelection === 'paper'
         case 'paper':
             return computerSelection === 'rock'
-    }
+    };
 }
 
-document.body.addEventListener('click', (e) => {
-    playRound(e.target.name, computerPlay())
+function resetGame() {
+    playerButtonsContainer.style.display = 'block'
+    resetGameButton.style.display = 'none'
+    playerScoreSpan.textContent = '0'
+    computerScoreSpan.textContent = '0'
+    roundNumberSpan.textContent = '1'
+    roundResultDiv.textContent = ''
+    playerScore = 0;
+    computerScore = 0;
+    roundNumber = 1;
+    
+}
+
+playerButtonsContainer.addEventListener('click', (e) => {
+    roundNumberSpan.textContent = ++roundNumber
+    let playerSelection = e.target.name;
+
+    if (!playerSelection) return
+
+    let computerSelection = computerPlay()
+    let roundWinner = playRound(playerSelection, computerSelection);
+
+    if (roundWinner === 'player'){
+        playerScoreSpan.textContent = ++playerScore;
+        roundResultDiv.textContent = `You win this round! ${playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1)} beats ${computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1)}.`
+    } else if (roundWinner === 'computer'){
+        computerScoreSpan.textContent = ++computerScore;
+        roundResultDiv.textContent = `You lose this round. ${computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1)} beats ${playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1)}.`
+    } else {
+        roundResultDiv.textContent = `It's a tie this round! Both you and the computer chose ${playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1)}.`
+
+    }
+
+
+    if (playerScore >= 5 || computerScore >= 5){
+        playerButtonsContainer.style.display = 'none';
+        roundResultDiv.textContent = playerScore > computerScore ? `You won the game 🎉! Your score is ${playerScore} and the computer's is ${computerScore}.` : `You lost this time 😞. Your score is ${playerScore} and the computer's is ${computerScore}.`
+        resetGameButton.style.display = 'block';
+
+    }
 })
 
-
-// function game(){
-
-    // let playerScore = 0
-    // let computerScore = 0
-
-    // for(let i = 0; i < 5; i++){
-    //     let userInput = prompt('Choose rock, paper or scissors!');
-    //     let winner = playRound(userInput, computerPlay());
-
-    //     if (winner === 'player'){
-    //         playerScore += 1
-    //     } else if (winner === 'computer'){
-    //         computerScore += 1
-    //     }
-    // }
-
-    // if (playerScore === computerScore){
-    //     console.log(`Wow! The game ends at a tie at ${playerScore} - ${computerScore}!`)
-    // } else{ 
-    // console.log (
-    //     playerScore > computerScore ? `You won the game 🎉! Your score is ${playerScore} and the computer's is ${computerScore}.` : `You lost this time 😞. Your score is ${playerScore} and the computer's is ${computerScore}.` 
-    // )}
-// }
-
-// game()
+resetGameButton.addEventListener('click', resetGame)
